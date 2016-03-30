6c6779d026826ec3fc3ee7a3dfddd6719a929027
qiuxj <qiuxj@bitbucket.org>
2016-03-30 14:00:53 +0800

    add invoice tips, and move common js from layout.html to lib.js

--- public/js/lib.js
+++ public/js/lib.js
@@ -23,5 +23,68 @@
         self.parent().find(".dropdown-menu-selected").val($(this).attr("data-value")).trigger("input");
       });
     });
-  }
+  };
+  
+  $.fn.CustomAlert = function(msg){
+    if(!msg || !$(this).hasClass("modal")){
+      return;
+    }
+
+    $(this).find(".modal-body").text(msg);
+    $(this).modal();
+  };
+
+  $(".dropdown-menu").BootstrapDropDown();
+
+  var navOpen = false;
+
+  $(".hyf-navbar-toggle").click(function(){
+    navOpen = !navOpen;
+    if(navOpen){
+      $("html, body").css("overflow", "hidden");
+    }else{
+      $("html, body").removeAttr("style");
+    }
+    $(".hyf-navbar-overlay").toggleClass("hyf-hide");
+    $(".hyf-navbar-nav").toggleClass("hyf-hide");
+  });
+
+  $(".btn-hide-navbar").click(function(){
+    navOpen = false;
+    $("html, body").removeAttr("style");
+    $(".hyf-navbar-overlay").addClass("hyf-hide");
+    $(".hyf-navbar-nav").addClass("hyf-hide");
+  });
+
+  $.fn.hyfdropdown = function(){
+    return this.each(function(){
+      var self = $(this);
+      self.find(".hyf-dropdown-toggle").on("click", function(){
+        self.toggleClass("open");
+      });
+    });
+  };
+
+  var bodyClickHandler = function(e){
+    var target = e.target || e.touches[0].target;
+
+    if(target.tagName === 'A'){
+      return ;
+    }
+
+    var dropdown = $(target).parent();
+
+    $(".hyf-dropdown-sm").each(function(){
+      if(dropdown.is(this)){
+        return;
+      }
+
+      $(this).removeClass("open");
+    });
+  };
+
+  $("body").on("click", bodyClickHandler);
+  $("body").on("tap", bodyClickHandler);
+
+  $(".hyf-dropdown-sm").hyfdropdown();
 })(jQuery, window);
